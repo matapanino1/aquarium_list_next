9d0abe04e3102779e2a891b4c8ed63575d607ab9
matapanino <pghby87971@yahoo.co.jp>
2022-09-09 09:31:05 +0900

    6th commit

--- pages/list.js
+++ pages/list.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Head from 'next/head';
 import Container from '../components/aquarium-list/Container';
 
 const List = () => {
   return (
       <>
+          <Head>
+              <title>日本全国水族館リスト</title>
+              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+              <meta name="description" content="日本全国にある水族館をリスト化しました。旅先で遊ぶ場所に迷った時の参考にどうぞ。" />
+          </Head>
           <Container />
       </>
   )
