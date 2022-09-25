267574ab9f502afed3805a2e559372bb4a078e23
matapanino <pghby87971@yahoo.co.jp>
2022-09-25 14:05:59 +0900

    0925 best commit

--- pages/blog.js
+++ pages/blog.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { getSortedPostsData } from "../lib/posts";
 import Link from 'next/link';
-import MediaCard from "../components/blog-page/Card";
 
 const Blog = ({ allPostsData }) => {
   return (
     <ul>
       {allPostsData.map(({ id, date, title }) => (
         <li key={id}>
-          <MediaCard>
           <Link href={`/posts/${id}`}>
           <a>{title}</a>
           </Link>
           <br />
             {date}
-            </MediaCard>
         </li>
       ))}
     </ul>
